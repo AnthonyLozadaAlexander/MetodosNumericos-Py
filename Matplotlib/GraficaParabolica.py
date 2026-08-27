e9d108716d66a1cf35f1f210c0d4f3e11ce9dda3
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-27 02:01:34 -0500

    feat(Matplotlib): Refactor GraficaParabolica.py to use variables for coefficients

--- Matplotlib/GraficaParabolica.py
+++ Matplotlib/GraficaParabolica.py
@@ -2,11 +2,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+a: int = 1
+b: int = -7
+c: int = 12
+
 x: np.ndarray = np.linspace(0, 10, 50)
-y: np.ndarray = x**2 - 7 * x + 12
+y: np.ndarray = (a * x**2) + (b * x) + (c)
 
 plt.plot(x, y, marker="o", color="green")
-plt.title("f(x) = x^2 - 7x  + 12")
+plt.title(f"f(x) = {a}x^2 {b}x  + {c}")
 plt.xlabel("Eje X")
 plt.ylabel("Eje Y")
 
