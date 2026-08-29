83216df80dccc17ca3310b6dd4e25cedd5876499
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-29 17:23:40 -0500

    feat(Derivadas): Mejora el archivo E1.py con visualización de función y su derivada

--- src/Derivadas/E1.py
+++ src/Derivadas/E1.py
@@ -1,2 +1,17 @@
-import numpy  as np
+import numpy as np
 import matplotlib.pyplot as plt
+
+x: np.ndarray = np.linspace(-10, 10, 100)
+y: np.ndarray = x**2
+
+y_derivada: np.ndarray = -4 * x - 4  #  pendiente en el punto x = -2 y = 4
+
+plt.plot(x, y, label="f(x) = x^2")
+plt.plot(x, y_derivada, label="Derivada: y =  -4x - 4")
+plt.scatter(
+    -2, 4, color="red", s=100, zorder=5, label="Punto (-2, 4)"
+)  # punto en la funcion
+
+plt.legend()
+plt.grid(True)
+plt.show()
