a49c52debd7b512400b793b5cda3c650b01b0f3d
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-29 17:41:35 -0500

    feat(Derivadas): Agrega archivo E2.py para graficar función y su derivada

---
+++ src/Derivadas/E2.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x: np.ndarray = np.linspace(-2, 3, 100)
+y: np.ndarray = x**3 - 2 * x
+
+y_derivada: np.ndarray = 3 * x**2 - 2
+
+plt.plot(x, y, label="f(x) = x^3 - 2x")  # funcion f(x) = x^3 - 2x
+plt.plot(
+    x, y_derivada, label="Derivada:  y = 3x^2 - 2"
+)  # grafica la funcion de la derivada
+
+plt.scatter(1, -1, color="blue", s=50, zorder=5, label="Punto (1, -1)")
+# plt.scatter(1, 1, color="red", s=50, zorder=5, label="Punto (1, 1)")
+
+plt.legend()
+plt.grid(True)
+plt.show()
