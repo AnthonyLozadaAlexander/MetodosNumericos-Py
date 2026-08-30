be4751b47b6e6f2c1e21a4b636a1426583078583
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-30 01:09:54 -0500

    feat(Derivadas): Ajusta rango de x en E2.py para mejorar la visualización

--- src/Derivadas/E2.py
+++ src/Derivadas/E2.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x: np.ndarray = np.linspace(-4, 4, 100)
-y: np.ndarray = x**3 - 2 * x
+x: np.ndarray = np.linspace(-2, 3, 100)
+y: np.ndarray = (x**3) - 2 * x
 
 y_derivada: np.ndarray = x - 2  # recta tangente de x^3 - 2x
 
