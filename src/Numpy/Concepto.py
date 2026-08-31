02e920b6bfe03b9274273e72e9bc049a72902b82
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-31 02:48:13 -0500

    fix(Concepto.py): Update vector visualization color and enhance plot details

--- src/Numpy/Concepto.py
+++ src/Numpy/Concepto.py
@@ -8,4 +8,17 @@ magnitud: float = np.linalg.norm(v)
 
 print(f"Magnitud: {magnitud}")
 
-plt.quiver(0, 0, v[0], v[1], angles="xy", scale_units="xy", scale=1, color="green")
+plt.quiver(0, 0, v[0], v[1], angles="xy", scale_units="xy", scale=1, color="Blue")
+
+
+plt.xlim(-1, 5)
+plt.ylim(-1, 5)
+
+plt.axhline(0, color="black", linewidth=0.5)
+plt.axvline(0, color="black", linewidth=0.5)
+plt.scatter(v[0], v[1], color="Red", label=f"x = {v[0]}, y = {v[1]}")
+plt.legend()
+plt.grid()
+
+plt.title("Vector V = [4, 3]")
+plt.show()
