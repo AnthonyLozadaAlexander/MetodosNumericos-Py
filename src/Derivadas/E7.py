a2121918a0c26c3d3eaf624262adf81f7b33a4cd
zDefcon21 <antholozadarequelme@gmail.com>
2026-08-31 02:30:52 -0500

    fix(E7.py): Update plot labels for clarity and consistency

--- src/Derivadas/E7.py
+++ src/Derivadas/E7.py
@@ -30,8 +30,8 @@ y: np.ndarray = F(xGen)  # funcion f(x) = x**2 * np.exp(x)
 
 y_derivada: np.ndarray = m_num * (xGen - x1) + fx  # recta tangente de x**2 * np.exp(x)
 
-plt.plot(xGen, y, label="Tiempo(x) = x^2e^x")
-plt.plot(xGen, y_derivada, label="Derivada: y = e^x(x^2 + 2x)")
+plt.plot(xGen, y, label="Megabytes(t) = t^2e^t")
+plt.plot(xGen, y_derivada, label="Derivada: y = e^t(t^2 + 2t)")
 plt.axhline(0, color="Black", linewidth=1)  # linea que cruza el eje x
 plt.axvline(0, color="Black", linewidth=1)  # linea que cruza el eje y
 
